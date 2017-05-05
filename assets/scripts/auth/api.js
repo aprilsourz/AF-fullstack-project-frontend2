98cc7738eb9df7b7a5e93c0587e8a8195fec1c3e
arlofeirman <arlofeirman@gmail.com>
2017-05-05 11:23:05 -0400

    adds ajax for auth

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,49 @@
+const config = require('../config.js')
+const store = require('../store.js')
+
+const signUp = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up',
+    method: 'POST',
+    data
+    // data: data
+  })
+}
+
+const signIn = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in',
+    method: 'POST',
+    data
+  // data: data
+  })
+}
+
+const changePassword = (data) => {
+  return $.ajax({
+    url: config.apiOrigin + '/change-password/' + store.user.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+    // data: data
+  })
+}
+
+const signOut = () => {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
